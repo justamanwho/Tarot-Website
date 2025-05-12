b53109e7355018ad7ddf724d4c47b46f33a8007b
justamanwho <dmytro.kostuyk@gmail.com>
2025-05-12 22:10:25 +0200

    added logging/simplified app.route decorators/made language switching faster

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask, render_template, redirect, url_for, session, request, jsonify
+from flask import Flask, render_template, redirect, url_for, session
 from dotenv import load_dotenv
+import logging
+import atexit
 import json
 import os
 
 
+logger = logging.getLogger(__name__)
+logger.setLevel('DEBUG')
+
+handlers = [logging.StreamHandler(), logging.FileHandler('logs.log')]
+formatter = logging.Formatter('%(levelname)s | %(name)s | %(asctime)s | %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+for handler in handlers:
+    handler.setLevel('DEBUG')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+
 app = Flask(__name__)
+logger.info("Website is live")
+
+
+def app_shutdown():
+    logger.info("Website is shut down")
+
+
+atexit.register(app_shutdown)
+
+
 load_dotenv('.env')
 app.secret_key = os.getenv('SECRET_KEY')
 
@@ -20,7 +44,9 @@ LANGUAGE_OPTIONS = {
 def load_translations(lang_code):
     file_path = f"translations/{lang_code}.json"
     if not os.path.exists(file_path):
-        file_path = "translations/ukr.json"  # Default Language
+        # Default Language
+        file_path = "translations/ukr.json"
+
     with open(file_path, 'r', encoding='utf-8') as f:
         return json.load(f)
 
@@ -28,7 +54,8 @@ def load_translations(lang_code):
 @app.route('/')
 def index():
     if 'lang' not in session:
-        session['lang'] = 'ukr' # Defaul Language
+        # Default Language
+        session['lang'] = 'ukr'
 
     translations = load_translations(session['lang'])
     max_length = max(len(option['name']) for option in LANGUAGE_OPTIONS.values())
@@ -38,23 +65,31 @@ def index():
                            language_options=LANGUAGE_OPTIONS,
                            max_length=max_length)
 
-@app.route('/set_language/<lang_code>')
+
+@app.route('/<lang_code>')
 def set_language(lang_code):
     if lang_code in LANGUAGE_OPTIONS:
         session['lang'] = lang_code
+
+    logger.info(f"Language was set to {lang_code}")
+
     return redirect(url_for('index'))
 
 
 # References for website
 
-@app.route('/jolyne', methods=['GET'])
+@app.route('/jolyne')
 def jojo_reference_jolyne():
+    logger.info("Jolyne?")
+
     return '<img src="../static/jolyne.png" alt="Jolyne">'
 
 
-@app.route('/avdol', methods=['GET'])
-@app.route('/jojo-reference', methods=['GET'])
+@app.route('/avdol')
+@app.route('/jojo-reference')
 def jojo_reference_avdol():
+    logger.info("Avdol, how do I name my stand?")
+
     return '<img src="../static/avdol.png" alt="Avdol">'
 
 
